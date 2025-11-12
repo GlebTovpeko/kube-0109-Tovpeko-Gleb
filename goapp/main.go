746e6167cb6c3235331f9a48cae7c3dd44f6ac89
Gleb Tovpeko <glebtovpeko77@gmail.com>
2025-11-12 11:44:48 +0300

    12

---
+++ goapp/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go App!</h1>")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status": "healthy"}`)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/health", healthHandler)
+
+	// Убедитесь, что порт соответствует EXPOSE в Dockerfile
+	log.Println("Server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
